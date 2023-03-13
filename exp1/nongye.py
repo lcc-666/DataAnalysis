c225cc680494ecfdd685dd052dc9950508512eff
lcc-666 <2502247952@qq.com>
2023-03-13 20:37:16 +0800

    农业数据提取

--- exp1/nongye.py
+++ exp1/nongye.py
@@ -1,7 +1,8 @@
-f=open("toutiao_cat_data.txt","r")
-txt=f.readlines()
+# 扩展：1.读取toutiao_cat_data.txt文本，提取文本中与农业相关的新闻并保存在agriculture.txt。
+f = open("toutiao_cat_data.txt", "r")
+txt = f.readlines()
 f.close()
-f=open("agriculture.txt","w")
+f = open("agriculture.txt", "w")
 for i in txt:
     if "农" in i:
         f.write(i)
